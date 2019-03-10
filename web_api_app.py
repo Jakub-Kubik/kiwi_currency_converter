aa2b462b8de46ea675000b9a212d817d227980fb
Jan Kubik <jakupkubik@gmail.com>
2019-03-10 22:52:50 +0100

    api.py renamed

---
+++ web_api_app.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Author: Jakub Kubik
+# Date: 10.03.2019
+
+from flask import Flask, request, jsonify, render_template
+from argparse import Namespace
+from initial_task_package.arguments import check_currency_args_validity, adjust_from_to_currency
+from initial_task_package.download import download_exchange_rates
+from initial_task_package.conversion import convert
+
+GET_PARAM_ERR = 1
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.errorhandler(404)
+    def error(e):
+        if e == GET_PARAM_ERR:
+            return render_template('error.html', GET_error=True), 404
+        else:
+            return render_template('error.html', GET_error=False), 404
+
+    @app.route('/currency_converter')
+    def currency_converter():
+        args = Namespace(amount=request.args.get('amount'),
+                         input_currency=request.args.get('input_currency'),
+                         output_currency=request.args.get('output_currency'))
+        if not args.amount or not args.input_currency:
+            return error(GET_PARAM_ERR)
+        try:
+            amount = float(args.amount)
+        except ValueError:
+            return error(GET_PARAM_ERR)
+        if check_currency_args_validity(args) is False or amount < 0:
+            return error(GET_PARAM_ERR)
+
+        currency_from, currency_to = adjust_from_to_currency(args.input_currency, args.output_currency)
+
+        exchange_rates = download_exchange_rates()
+
+        converted_currency = convert(amount, currency_from, currency_to, exchange_rates)
+
+        return jsonify({'input': {'amount': amount, 'currency_from': currency_from},
+                        'output': converted_currency})
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(port=5000)
