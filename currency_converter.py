63a3d53debf047d0d414294e4491c9a610af53c6
Jan Kubik <jakupkubik@gmail.com>
2019-03-11 19:19:51 +0100

    sys.exit changed to raise SystemExit

--- currency_converter.py
+++ currency_converter.py
@@ -5,23 +5,23 @@
 from functions.arguments import parse_arguments, check_currency_args_validity, adjust_from_to_currency
 from functions.download import download_exchange_rates
 from functions.conversion import convert
-from sys import exit, stderr
+from sys import stderr
 from json import dumps
 
 
 if __name__ == '__main__':
     args = parse_arguments()
     if check_currency_args_validity(args) is False or args.amount < 0:
-        stderr.write('Wrong arguments')
-        exit(2)
+        stderr.write('Wrong arguments!\n')
+        raise SystemExit(2)
 
     amount = args.amount
     currency_from, currency_to = adjust_from_to_currency(args.input_currency, args.output_currency)
 
     exchange_rates = download_exchange_rates()
     if exchange_rates is None:
-        stderr.write('Can\'t download today\'s currency rates')
-        exit(3)
+        stderr.write('Can\'t download today\'s currency rates!\n')
+        raise SystemExit(3)
 
     converted_currency = convert(amount, currency_from, currency_to, exchange_rates)
 
